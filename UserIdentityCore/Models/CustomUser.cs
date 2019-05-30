2d5aaa5af580a0f95ad87fc733550872ed2ff8b6
Sandiso Ngcwangushe <sbngcwangushe@gmail.com>
2019-05-30 12:02:01 +0200

    Add project files.

---
+++ UserIdentityCore/Models/CustomUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserIdentityCore.Models
+{
+    public class CustomUser
+    {
+        public CustomUser() : base()
+        {
+
+        }
+
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
---
+++ UserIdentityCore/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserIdentityCore.Models
+{
+    public class Employee
+    {
+    }
+}
