f334c128dc128f955c91e7660f03910d276cb938
Sandiso Ngcwangushe <sbngcwangushe@gmail.com>
2019-05-30 12:19:23 +0200

    -Add CustomUser class  to  Models extends IdentityUser -Add CustomRole class  to  Models extends IdentityRole -Add Employee and Coresponding Data to  Models

--- UserIdentityCore/Models/Address.cs
+++ UserIdentityCore/Models/Address.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace UserIdentityCore.Models
 {
+    [Table("Address")]
     public class Address
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string HouseNumber { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+
     }
 }
---
+++ UserIdentityCore/Models/CustomRole.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserIdentityCore.Models
+{
+    public class CustomRole : IdentityRole
+    {
+        public CustomRole() : base() { }
+        public CustomRole(string roleName) : base(roleName) { }
+
+        public CustomRole(string roleName, string decription, DateTime creationDate) : base(roleName)
+        {
+            this.Description = decription;
+            this.CreationDate = creationDate;
+        }
+
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
